46db80a933b7f466054f6f2d964d5d45fcbbee50
Mona Lisa <yurymp89@gmail.com>
2022-10-09 23:50:46 -0500

    Se adicionaron todos los archivos del proyecto

---
+++ Index.js
@@ -0,0 +1,36 @@
+
+function multiplicar() {
+    let num1 = document.getElementById("uno").value;
+    let num2 = document.getElementById("dos").value;
+    document.getElementById("resultado").innerHTML = num1 * num2;
+}
+
+function dividir() {
+    let num1 = document.getElementById("uno").value;
+    let num2 = document.getElementById("dos").value;
+    if(num2==0){
+        document.getElementById("resultado").innerHTML = "La división por cero no existe";
+    }else{
+        document.getElementById("resultado").innerHTML = num1 / num2;
+    }
+    
+}
+
+
+function fechaHora(){
+    document.getElementById('hora').innerHTML=Date();
+}
+
+
+    let texto = "\tBuenos días chicos";
+    let longitudd = texto.length;
+    document.getElementById('longitud').innerHTML=longitudd + texto;
+    document.getElementById('test').innerHTML=texto;
+
+const estudiantes = {
+    nombre:"Andrés",
+    apellido:"Perez",
+    edad:45
+}
+document.getElementById('estudiante').innerHTML=estudiantes.nombre + " " 
++estudiantes.apellido + " " +estudiantes.edad;
